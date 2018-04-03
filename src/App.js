4e75c57ae46d9a41e57e8b6e969875e728b34026
Jefferson Griebel <jeffersongriebel@JeffersonsMBP2.itpro.tv>
2018-04-03 18:36:55 -0400

    created home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+//components
+import Home from './components/Home';
 
 class App extends Component {
   render() {
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = props => (
+    <div>
+        this is your home!
+    </div>
+)
+
+export default Home;
