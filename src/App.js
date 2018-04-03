03a197d55e43f60512868ba325a1624d68b47806
Jefferson Griebel <jeffersongriebel@JeffersonsMBP2.itpro.tv>
2018-04-03 18:39:18 -0400

    created a Profile component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 //components
 import Home from './components/Home';
+import Profile from './components/Profile';
 
 class App extends Component {
   render() {
     return (
      <div>
        hello world
+       <Home />
+       <Profile />
      </div>
     );
   }
